72a6532b24b42656076fadc4007a71d790c97126
Louise EGAIN <louiseegain2401@gmail.com>
2023-04-08 20:56:06 +0200

    Mise à jour environnement
    Suite à la revue du cahier des charges, l'environnement a été mis à jour dans ce sens

--- evenement.cob
+++ evenement.cob
@@ -6,7 +6,7 @@
       ******************************************************************
        IDENTIFICATION DIVISION.
       *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
-       PROGRAM-ID. YOUR-PROGRAM-NAME.
+       PROGRAM-ID. Evenements.
        ENVIRONMENT DIVISION.
       *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
        CONFIGURATION SECTION.
@@ -16,75 +16,94 @@
            select futilisateur assign to "utilisateur.dat"
            organization indexed
            access mode is dynamic
-           record key is futi_login
-           alternate record key is futi_genre WITH DUPLICATES
-           ALTERNATE RECORD KEY IS futi_type WITH DUPLICATES
-           file status is cr_futi.
+           record key is futil_login
+           alternate record key IS futil_mail
+           alternate record key IS futil_tel
+      *    alternate record key is futil_genre WITH DUPLICATES
+           ALTERNATE RECORD KEY IS futil_type WITH DUPLICATES
+           ALTERNATE RECORD KEY IS futil_formation WITH DUPLICATES
+           file status is cr_futil.
 
 
            select fevenement assign to "evenement.dat"
            organization indexed
            access mode is dynamic
-           record key is fevent_id
+           record key is fevent_nom
            alternate record key is fevent_type WITH DUPLICATES
-           alternate record key is fevent_idOrga WITH DUPLICATES
+           ALTERNATE RECORD KEY IS fevent_date WITH DUPLICATES
+           alternate record key is fevent_loginOrga WITH DUPLICATES
+           ALTERNATE RECORD KEY IS fevent_etat WITH DUPLICATES
            file status is cr_fevent.
 
 
            SELECT fparticipant ASSIGN TO "participant.dat"
            ORGANIZATION INDEXED
            ACCESS MODE IS DYNAMIC
-           RECORD KEY fpart_inscriptionId
-           ALTERNATE RECORD KEY fpart_inscriptionEtat WITH DUPLICATES
-           ALTERNATE RECORD KEY fpart_login WITH DUPLICATES
-           ALTERNATE RECORD KEY fpart_idEvent WITH DUPLICATES
+           RECORD KEY fpart_cle
+           ALTERNATE RECORD KEY IS fpart_login
+           ALTERNATE RECORD KEY IS fpart_nomEvent WITH DUPLICATES
+           ALTERNATE RECORD KEY IS fpart_etat WITH DUPLICATES
            FILE STATUS IS cr_fpart.
 
            SELECT fhistorique ASSIGN TO "historique.dat"
-           ORGANIZATION SEQUENTIAL
-           ACCESS MODE IS SEQUENTIAL
-           FILE STATUS IS cr_fhisto
+           ORGANIZATION INDEXED
+           ACCESS MODE IS DYNAMIC
+           RECORD KEY fhisto_nom
+           ALTERNATE RECORD KEY fhisto_type WITH DUPLICATES
+           ALTERNATE RECORD KEY fhisto_date WITH DUPLICATES
+           ALTERNATE RECORD KEY fhisto_etat WITH DUPLICATES
+           ALTERNATE RECORD KEY fhisto_participants WITH DUPLICATES
+           FILE STATUS IS cr_fhisto.
+
       *-----------------------
        DATA DIVISION.
       *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
        FILE SECTION.
        FD futilisateur.
        01 tamp_futi.
-           02 futi_nom PIC A(30).
-           02 futi_prenom PIC A(30).
-           02 futi_login PIC X(30).
-           02 futi_mdp PIC X(12).
-           02 futi_genre PIC A(8).
-           02 futi_type PIC A(10).
+           02 futil_nom PIC A(30).
+           02 futil_prenom PIC A(30).
+           02 futil_login PIC X(30).
+           02 futil_mdp PIC X(12).
+           02 futil_mail PIC X(30).
+           02 futil_tel PIC X(14).
+      *     02 futil_genre PIC A(8).
+           02 futil_type PIC A(10).
+           02 futil_formation PIC A(20).
+           02 futil_naissance PIC X(10).
 
        FD fevenement.
        01 tamp_fevent.
-           02 fevent_id PIC 9(8).
+           02 fevent_nom PIC A(20).
            02 fevent_type PIC A(20).
            02 fevent_date PIC X(10).
-           02 fevent_idOrga PIC X(30).
+           02 fevent_loginOrga PIC X(30).
            02 fevent_description PIC X(50).
            02 fevent_adresse PIC X(30).
+           02 fevent_etat PIC A(8).
+           02 fevent_seuil PIC 9(3).
+           02 fevent_heure PIC X(5).
 
        FD fparticipant.
        01 tamp_fpart.
-           02 fpart_inscriptionId PIC 9(8).
-           02 fpart_inscriptionEtat PIC 9(10).
-           02 fpart_inscriptionDate PIC X(10).
-           02 fpart_login PIC X(30).
-           02 fpart_idEvent PIC 9(8).
+           02 fpart_cle.
+               03 fpart_login PIC X(30).
+               03 fpart_nomEvent PIC A(20).
+           02 fpart_etat PIC A(10).
 
        FD fhistorique.
        01 tamp_fhisto.
-           02 fhisto_eventId PIC 9(8).
-           02 fhisto_eventType PIC A(20).
-           02 fhisto_eventDate PIC X(10).
-           02 fhisto_eventIdOrga PIC X(30).
+           02 fhisto_nom PIC A(30).
+           02 fhisto_type PIC A(20).
+           02 fhisto_date PIC X(10).
+           02 fhisto_loginOrga PIC X(30).
            02 fhisto_description PIC X(50).
-           fhisto_nbPart PIC 9(3).
+           02 fhisto_adresse PIC X(50).
+           02 fhisto_etat PIC A(8).
+           02 fhisto_participants PIC 9(3).
       *-----------------------
        WORKING-STORAGE SECTION.
-       77 cr_futi PIC 9(2).
+       77 cr_futil PIC 9(2).
        77 cr_fevent PIC 9(2).
        77 cr_fpart PIC 9(2).
        77 cr_fhisto PIC 9(2).
@@ -95,7 +114,36 @@
       **
       * The main procedure of the program
       **
-            DISPLAY "Hello world"
+
+
+      *-----------------------------------------------------------------
+      *                  CREATION DES FICHIERS
+      *-----------------------------------------------------------------
+           OPEN I-O futilisateur
+           IF cr_futil =35 THEN
+               OPEN OUTPUT futilisateur
+           END-IF
+           CLOSE futilisateur
+
+           OPEN I-O fevenement
+           IF cr_fevent =35 THEN
+               OPEN OUTPUT fevenement
+           END-IF
+           CLOSE fevenement
+
+           OPEN I-O fparticipant
+           IF cr_fpart =35 THEN
+               OPEN OUTPUT fparticipant
+           END-IF
+           CLOSE fparticipant
+
+           OPEN I-O fhistorique
+           IF cr_fhisto =35 THEN
+               OPEN OUTPUT fhistorique
+           END-IF
+           CLOSE fhistorique
+
+
             STOP RUN.
       ** add other procedures here
-       END PROGRAM YOUR-PROGRAM-NAME.
+       END PROGRAM Evenements.
